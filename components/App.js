3b3bcc1113c72efd358c60b157fcbdf0a9624b6d
Alex Hays <alexhays@gmail.com>
2018-01-16 12:02:13 -0500

    basic starting point for react apps

---
+++ components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {  
+    return (
+      <div>
+        Well, well, well
+      </div>
+    )
+  }
+}
+
+export default App
