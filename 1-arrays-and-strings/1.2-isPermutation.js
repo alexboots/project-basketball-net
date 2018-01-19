a8cf097089d7d77a261470fc765f4120af7cbc18
Alex Hays <alexhays@gmail.com>
2018-01-19 06:53:34 -0500

    Revert "add palindrome test"
    This reverts commit bc5a1f3d7ffc472d57dee587cf1a256fcd6fa5e2.

--- 1-arrays-and-strings/1.2-isPermutation.js
+++
@@ -1,9 +0,0 @@
-// Given two strings, check to see if one is a permutation of the other
-export function isPermutation(stringOne, stringTwo) {
-  const oneSorted = stringOne.split('').sort().slice()
-  const twoSorted = stringTwo.split('').sort().slice()
-
-  return oneSorted.some((letter, index) => {
-    return twoSorted[index] === letter ? true : false
-  })
-}
--- 1-arrays-and-strings/1.3-URLify.js
+++
@@ -1,4 +0,0 @@
-// replace spaced with %20 
-export function URLify(string) {
-  return string.split(" ").join("%20")
-} 
