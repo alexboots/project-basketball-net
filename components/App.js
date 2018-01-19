67846274c9a43fcf62ce745bc1cf8042866f7804
Alex Hays <alexhays@gmail.com>
2018-01-19 07:25:54 -0500

    add LESS

--- components/App.js
+++ components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import './App.less'
 
 class App extends Component {
   render() {  
     return (
-      <div>
+      <div className="hello">
         Well, well, well
       </div>
     )
