014e221ed751180a89d7b0c30ab0daafa8dbebb1
Alex Hays <alexhays@gmail.com>
2018-02-21 20:44:27 -0500

    get basic map on page

--- components/App.js
+++ components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './App.less'
 
+import MapContainer from './Map/MapContainer'
+
 class App extends Component {
   render() {  
     return (
-      <div className="hello">
-        Well, well, well
+      <div className="hello" style={{width: '100%', height: '400px'}}>
+        <MapContainer />
       </div>
     )
   }
--- components/Map/MapContainer.js
+++ components/Map/MapContainer.js
@@ -1,1 +1,36 @@
-MapContainer.js
+import React, { Component } from 'react'
+
+import GoogleMapReact from 'google-map-react'
+
+const AnyReactComponent = ({ text }) => (
+  <div style={{
+    position: 'relative', color: 'white', background: 'red',
+    height: 40, width: 60, top: -20, left: -30,    
+  }}>
+    {text}
+  </div>
+);
+
+class MapContainer extends Component {
+  render() {
+    const defaultProps = {
+      center: {lat: 59.95, lng: 30.33},
+      zoom: 11
+    }
+
+    return (
+       <GoogleMapReact
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent 
+          lat={59.955413} 
+          lng={30.337844} 
+          text={'Kreyser Avrora'} 
+        />
+      </GoogleMapReact>
+    );
+  }
+}
+
+export default MapContainer
